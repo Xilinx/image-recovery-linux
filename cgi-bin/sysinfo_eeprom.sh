93741c03399b18ce47bd42d4243f122b13a88b89
Parthiban Kanchipuram <Parthiban.Kanchipuram@amd.com>
2025-11-04 11:03:25 +0530

    cgi-bin: Sanitize EEPROM string fields
    - Add tr filter to strip non-printable chars from EEPROM info fields
    
    Signed-off-by: Parthiban Kanchipuram <Parthiban.Kanchipuram@amd.com>
    Acked-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>

--- cgi-bin/sysinfo_eeprom.sh
+++ cgi-bin/sysinfo_eeprom.sh
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: MIT
 
 cat /sys/bus/i2c/devices/*/eeprom > sysinfo.bin
-brdnm=$(hexdump -s 0x16 -n6 -e '8/1 "%c"' sysinfo.bin | tr -d '\000')
-revnum=$(hexdump -s 0x44 -n8 -e '8/1 "%c"' sysinfo.bin | tr -d '\000')
-srlnum=$(hexdump -s 0x27 -n16 -e '8/1 "%c"' sysinfo.bin | tr -d '\000')
-prtnum=$(hexdump -s 0x38 -n9 -e '8/1 "%c"' sysinfo.bin | tr -d '\000')
-uuid=$(hexdump -s 0x56 -n16 -e '8/1 "%X"' sysinfo.bin | tr -d '\000')
+brdnm=$(hexdump -s 0x16 -n6 -e '8/1 "%c"' sysinfo.bin | tr -d '\000' | tr -cd 'A-Za-z0-9 ._-')
+revnum=$(hexdump -s 0x44 -n8 -e '8/1 "%c"' sysinfo.bin | tr -d '\000' | tr -cd 'A-Za-z0-9 ._-')
+srlnum=$(hexdump -s 0x27 -n16 -e '8/1 "%c"' sysinfo.bin | tr -d '\000' | tr -cd 'A-Za-z0-9 ._-')
+prtnum=$(hexdump -s 0x38 -n9 -e '8/1 "%c"' sysinfo.bin | tr -d '\000' | tr -cd 'A-Za-z0-9 ._-')
+uuid=$(hexdump -s 0x56 -n16 -e '8/1 "%X"' sysinfo.bin | tr -d '\000' | tr -cd 'A-Za-z0-9 ._-')
 rm sysinfo.bin
 
 echo "Content-type: text/html"
